ed691bba0be6f975ceb67e6ded53d6a1a0c3e79e
VaneGarciaYara <vanegarciay@gmail.com>
2017-06-02 00:38:49 -0400

    arreglo en estilos

--- js/app.js
+++ js/app.js
@@ -7,12 +7,15 @@
             modal.innerHTML = "";
 
             textModal = document.createElement("h1");
+            textModal.classList.add("center");
+            textModal.setAttribute("id", "titulo_modal");
             texto = document.createTextNode("PROJECT TITLE");
             textModal.appendChild(texto);
             modal.appendChild(textModal);
 
             estrella = document.createElement("div");
             estrella.classList.add("linea-estrella-negra");
+            estrella.setAttribute("id", "estrella_modal");
             textModal.appendChild(estrella);
 
             bodyModal = document.createElement("div");
@@ -28,7 +31,7 @@
             modal.appendChild(close);
 
             parrafo = document.createElement("p");
-            textoP = document.createTextNode("Use this area of the page to describe your project. The icon above is part of a free icon set by Flat Icons. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!  Client: Start Bootstrap  Date: April 2014  Service: Web Development");
+            textoP = document.createTextNode("Use this area of the page to describe your project. The icon above is part of a free icon set by Flat Icons. On their website, you can download their free set with 16 icons, or you can purchase the entire set with 146 icons for only $12!  Client: Start Bootstrap  ");
             parrafo.appendChild(textoP);
             bodyModal.appendChild(parrafo);
 
